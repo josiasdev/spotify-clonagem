e1751c59c5a76e94b0b078e70ff16e2e27930d6c
josiasdev <josiasmartins098@gmail.com>
2024-11-02 15:00:01 -0300

    feat: Componente Main onde chama os outros componentes feitos até agora

---
+++ src/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import Sidebar from "../Sidebar/Sidebar";
+import './Main.css';
+const Main = () => {
+    return (
+        <>
+            <Header></Header>
+            <Sidebar></Sidebar>
+        </>
+    )
+}
+
+export default Main;
